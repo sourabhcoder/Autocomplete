0fa182d292733d427d9bf408369c25d0803b9376
Sourabh Kumar Sharma <sourabhkumarsharma778@gmail.com>
2017-10-27 13:49:12 +0530

    Updated the SQL Query
    modified the select query to get only the products that have the catalog,search enabled.

--- Model/ResourceModel/Searchterm.php
+++ Model/ResourceModel/Searchterm.php
@@ -22,7 +22,7 @@ class Searchterm extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 
     public function getProductData($params)
     {
-        $stmt = $this->getConnection()->query("SELECT entity_id FROM " . $this->getTable('catalog_product_entity_varchar') . " as cpev WHERE cpev.value LIKE '%".$params['searchterm']."%'  ANd cpev.attribute_id = 73");
+        $stmt = $this->getConnection()->query("SELECT cpev.entity_id as entity_id FROM " . $this->getTable('catalog_product_entity_varchar') . " as cpev INNER JOIN  " . $this->getTable('catalog_product_entity_int') . " as cpei ON cpev.entity_id = cpei.entity_id  WHERE cpev.value LIKE '%".$params['searchterm']."%'  ANd cpev.attribute_id = (SELECT attribute_id FROM  " . $this->getTable('eav_attribute') . " as ea WHERE ea.attribute_code = 'name' AND ea.entity_type_id = 4 AND cpei.attribute_id = (SELECT eavn.attribute_id FROM ".$this->getTable('eav_attribute')." as eavn WHERE attribute_code = 'visibility' AND entity_type_id = 4 ) )  AND cpei.value = 4 ");
         $result = $stmt->fetchAll();
         return $result;
     }
