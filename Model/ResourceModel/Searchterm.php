38163edb0f089be2e6cd57d5499ff8800a8dd88e
Sourabh Kumar Sharma <sourabhkumarsharma778@gmail.com>
2017-10-27 17:12:56 +0530

    Updated SQL Script
    Updated SQL Script to show store and default store products

--- Model/ResourceModel/Searchterm.php
+++ Model/ResourceModel/Searchterm.php
@@ -43,8 +43,7 @@ class Searchterm extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
             }
 
         }
-        $stmt = $this->getConnection()->query("SELECT cpev.entity_id as entity_id FROM " . $this->getTable('catalog_product_entity_varchar') . " as cpev INNER JOIN  " . $this->getTable('catalog_product_entity_int') . " as cpei ON cpev.entity_id = cpei.entity_id  WHERE cpev.value LIKE '%".$params['searchterm']."%'  ANd cpev.attribute_id = (SELECT attribute_id FROM  " . $this->getTable('eav_attribute') . " as ea WHERE ea.attribute_code = 'name' AND ea.entity_type_id = 4) AND cpei.attribute_id = (SELECT eavn.attribute_id FROM ".$this->getTable('eav_attribute')." as eavn WHERE eavn.attribute_code = 'visibility' AND eavn.entity_type_id = 4 )   AND cpei.value = 4  AND cpev.store_id = '".$storeId."' AND cpei.store_id = '".$storeId."' ");
-        
+        $stmt = $this->getConnection()->query("SELECT cpev.entity_id as entity_id FROM " . $this->getTable('catalog_product_entity_varchar') . " as cpev INNER JOIN  " . $this->getTable('catalog_product_entity_int') . " as cpei ON cpev.entity_id = cpei.entity_id  WHERE cpev.value LIKE '%".$params['searchterm']."%'  ANd cpev.attribute_id = (SELECT attribute_id FROM  " . $this->getTable('eav_attribute') . " as ea WHERE ea.attribute_code = 'name' AND ea.entity_type_id = 4) AND cpei.attribute_id = (SELECT eavn.attribute_id FROM ".$this->getTable('eav_attribute')." as eavn WHERE eavn.attribute_code = 'visibility' AND eavn.entity_type_id = 4 )   AND cpei.value = 4  AND (cpev.store_id = ".$storeId." OR cpev.store_id = 0  ) AND (cpei.store_id = '".$storeId."' OR cpei.store_id = 0 ) "); 
         $result = $stmt->fetchAll();
         return $result;
     }
